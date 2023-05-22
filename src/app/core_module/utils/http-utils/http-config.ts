2ebcaebafe9202d7d05ff2fc2e6da00e398e5e3c
aviyan.dahal <aviyan.dahal@sbsolutionsnepal.com>
2023-05-22 17:34:23 +0545

    first commit

---
+++ src/app/core_module/utils/http-utils/http-config.ts
@@ -0,0 +1,7 @@
+import { environment } from 'src/environments/environment';
+
+export class HttpConfig {
+  public static baseApiUrl = environment.baseUrl;
+  // public static baseApiUrl = environment.firebase.baseApiUrl;
+  public static auth = HttpConfig.baseApiUrl + '/login';
+}
